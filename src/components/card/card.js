1996a18ce56d3cd99a7c6c5ecc71a3f6ec68690c
elishamutang <elishamutang@outlook.com>
2025-05-14 00:41:16 +0800

    update: Updated card component complete with modal.

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -12,11 +12,35 @@ export default function CardComp({...props}) {
             <h1 class="title">${props.title}</h1>
             <p class="description">${props.description}</p>
             <div class="voteBtns">
-                <button type="button" class="vote-btn"><i class="fa-solid fa-caret-up fa-2xl"></i></button>
+                <button type="button" class="vote-btn up"><i class="fa-solid fa-caret-up fa-2xl"></i></button>
                 <p class="total-votes">${totalVotes}</p>
-                <button type="button" class="vote-btn"><i class="fa-solid fa-caret-down fa-2xl"></i></button>
+                <button type="button" class="vote-btn down"><i class="fa-solid fa-caret-down fa-2xl"></i></button>
             </div>
-            <a class="button cta" href=${props.button.url} target="_blank">${props.button.label}</a>
+            <div class="action-btns">
+                <a class="button cta" href=${props.button.url} target="_blank">${props.button.label}</a>
+                <dialog closedby="closerequest" id="${props.id}">
+                    <form class="edit-content">
+                        <h2 class="modal-title">Edit Card</h2>
+                    
+                        <label for="title">Title: </label>
+                        <input type="text" name="title" value="${props.title}"/>
+                        
+                        <label for="description">Description: </label>
+                        <input type="text" name="description" value="${props.description}"/>
+                        
+                        <label for="button-label">Button Label: </label>
+                        <input type="text" name="button-label" value="${props.button.label}"/>
+                        <div class="form-btns">
+                            <button type="submit" class="save-changes">Save</button>
+                            <button type="button" class="cancel-changes">Cancel</button>
+                        </div>
+                    </form>
+                    
+                </dialog>
+                <button type="button" class="open-modal">
+                    <i class="fa-solid fa-pen fa-xl"></i>
+                </button>
+            </div>
         </div>
         
     `
