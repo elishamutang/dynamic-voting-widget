156682e02eb5102b9aea19aaa325408ecd1ac0ca
elishamutang <elishamutang@outlook.com>
2025-05-15 23:49:48 +0800

    update: Updated Card component.

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,36 +1,36 @@
-export default function CardComp({...props}) {
+export default function CardComp(id, data) {
 
     const card = document.createElement('div')
     card.className = 'card'
-    card.id = `card-${props.id}`
+    card.id = `card-${id}`
 
-    const totalVotes = props.up - props.down
+    const totalVotes = data.votes.up - data.votes.down
 
     card.innerHTML = `
-        <img class="card-img" src=${props.src} alt="qr-code"/>
+        <img class="card-img" src=${data.image} alt="qr-code"/>
         
         <div class="content">
-            <h1 class="title">${props.title}</h1>
-            <p class="description">${props.description}</p>
+            <h1 class="title">${data.title}</h1>
+            <p class="description">${data.description}</p>
             <div class="voteBtns">
                 <button type="button" class="vote-btn up"><i class="fa-solid fa-caret-up fa-2xl"></i></button>
                 <p class="total-votes">${totalVotes}</p>
                 <button type="button" class="vote-btn down"><i class="fa-solid fa-caret-down fa-2xl"></i></button>
             </div>
             <div class="action-btns">
-                <a class="button cta" href=${props.button.url} target="_blank">${props.button.label}</a>
-                <dialog closedby="closerequest" id="dialog-${props.id}">
-                    <form class="edit-content" action="/" id="form-${props.id}">
+                <a class="button cta" href=${data.button.url} target="_blank">${data.button.label}</a>
+                <dialog closedby="closerequest" id="dialog-${id}">
+                    <form class="edit-content" action="/" id="form-${id}">
                         <h2 class="modal-title">Edit Card</h2>
                     
                         <label for="title">Title: </label>
-                        <input type="text" name="title" value="${props.title}"/>
+                        <input type="text" name="title" value="${data.title}" required maxlength="20"/>
                         
                         <label for="description">Description: </label>
-                        <input type="text" name="description" value="${props.description}"/>
+                        <input type="text" name="description" value="${data.description}" required/>
                         
                         <label for="button-label">Button Label: </label>
-                        <input type="text" name="buttonLabel" value="${props.button.label}"/>
+                        <input type="text" name="buttonLabel" value="${data.button.label}" required maxlength="20"/>
                         <div class="form-btns">
                             <button type="submit" class="save-changes">Save</button>
                             <button type="button" class="cancel-changes">Cancel</button>
