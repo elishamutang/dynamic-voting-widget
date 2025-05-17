e861f151f89f2c1032b9e92da1833c8d5db3211e
elishamutang <elishamutang@outlook.com>
2025-05-17 18:00:41 +0800

    update: Moved header to partials folder.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require __DIR__ .  '/../vendor/autoload.php';
 
-$loader = new \Twig\Loader\FileSystemLoader(['src/views', 'src/components']);
+$loader = new \Twig\Loader\FileSystemLoader(['..\src\views', '..\src\components']);
 $twig = new \Twig\Environment($loader, ['auto_reload' => true]);
 
 echo $twig->render('index.html.twig');
