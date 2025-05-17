81d9d497727efc425bb474fc1475121b505fd14b
elishamutang <elishamutang@outlook.com>
2025-05-18 01:00:34 +0800

    update: Re-factored index file.

--- public/index.php
+++ public/index.php
@@ -2,22 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Twig\Loader\FileSystemLoader;
 
 $loader = new FileSystemLoader(__DIR__ . '/views');
 $twig = new Environment($loader, ['auto_reload' => true]);
 
-$request = Request::createFromGlobals();
-
-$html = $twig->render('index.html.twig');
-
-$response = new Response($html);
-$response->send();
-
-//$loader = new \Twig\Loader\FileSystemLoader(['src/views', 'src/components']);
-//$twig = new \Twig\Environment($loader, ['auto_reload' => true]);
-//
-//echo $twig->render('index.html.twig');
+echo $twig->render('index.html.twig');
